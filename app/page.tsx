e732481b0c68f500e7a375b5079607a56be3346c
Aryan <paryan@digitalocean.com>
2025-02-19 22:33:19 +0000

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ import { FileUploader } from "./components/uploader/FileUploader";
 export default function Home() {
   return (
     <div className="mt-20 ml-40 w-[80%]">
-      <div>
-        <FileUploader apiUrl="/api/upload/" apiRedir="/" />
-      </div>
       <div className="mt-20">
         <p>Secondary</p>
         <FileUploader apiUrl="/api/reupload/" apiRedir="/" />
