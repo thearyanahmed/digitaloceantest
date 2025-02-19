f790486be4c753f3326221faf84d5a65ec3da711
Aryan <paryan@digitalocean.com>
2025-02-19 22:42:39 +0000

    WIP

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  serverActions: {
-    bodySizeLimit: '20mb' // Set desired value here
-  }
+
 };
 
 export default nextConfig;
